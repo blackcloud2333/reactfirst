18d71ef47b836f9911f37f69fee5c4c3981670a9
窦雅宁 <douyaning@qmtv.com>
2018-02-28 16:12:53 +0800

    '根据yeoman从ES5改为ES6'

--- generator-reactfirst/READEME.md
+++
@@ -1,5 +0,0 @@
-# generator-reactfist
-   准备对简单的专题页自动化生成而做的一个项目
-###使用方法
-    npm install
-   
---
+++ generator-reactfirst/generators/app/index.js
@@ -0,0 +1,13 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor (args, opts) {
+        super(args, opts);
+        };
+    writing () {
+        this.fs.copy(
+            this.templatePath('./'),
+            this.destinationPath(`src`)
+        );
+    }
+};
